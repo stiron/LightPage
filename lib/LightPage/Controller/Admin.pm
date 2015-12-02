34ef6025b5934a5d18091714e98ae1120d1ea526
Tamas Molnar <stiron@gmail.com>
2015-12-02 22:02:20 +0100

    Added user creation to the admin page

--- lib/LightPage/Controller/Admin.pm
+++ lib/LightPage/Controller/Admin.pm
@@ -25,14 +25,105 @@ sub index : Path : Args(0) {
     $c->stash( template => 'admin_page/admin_index.tt' );
 }
 
-=head2 user_list
+=head2 base
+
+Base for a chained action
 
 =cut
 
-sub user_list : Local : Args(0) {
+sub base : Chained('/') : PathPart('admin') : CaptureArgs(0) {
     my ( $self, $c ) = @_;
-    $c->stash( user_list => [ $c->model('DB::User')->all ] );
-    $c->stash( template  => 'admin_page/user_list.tt' );
+    $c->stash( resultset => $c->model('DB::User') );
+}
+
+=head2 base -> user_list
+
+Get the list of the users
+
+=cut
+
+sub user_list : Chained('base') : PathPart('user_list') : Args(0) {
+    my ( $self, $c ) = @_;
+    my $user_list = [ $c->model('DB::User')->all ];
+    $c->stash(
+        user_list => $user_list,
+        template  => 'admin_page/user_list.tt'
+    );
+}
+
+=head2 base -> user_object
+
+Get a user object id, stash it
+
+=cut
+
+sub user_object : Chained('base') : PathPart('id') : CaptureArgs(1) {
+    my ( $self, $c, $id ) = @_;
+    $c->stash( user_object => $c->stash->{resultset}->find($id) );
+    die "User ID $id not found!" if !$c->stash->{user_object};
+    $c->log->debug("*** INSIDE OBJECT METHOD for obj id=$id ***");
+}
+
+=head2 base -> create_user_form
+
+Form for creating a new user
+
+=cut
+
+sub create_user_form : Chained('base') : PathPart('create_user_form') : Args(0)
+{
+    my ( $self, $c ) = @_;
+    $c->stash( template => "admin_page/create_user_form.tt" );
+}
+
+=head2 base -> create_user_do
+
+Create a new user
+
+=cut
+
+sub create_user_do : Chained('base') : PathPart('create_user_do') : Args(0) {
+    my ( $self, $c ) = @_;
+
+    # Data from the form
+    my $username      = $c->request->params->{'username'};
+    my $first_name    = $c->request->params->{'first_name'};
+    my $last_name     = $c->request->params->{'last_name'};
+    my $email_address = $c->request->params->{'email_address'};
+    my $password      = $c->request->params->{'password'};
+    my $active        = 1;
+    my $role_id       = 1;
+
+    # Creating the user
+    my $user = $c->model('DB::User')->create(
+        {
+            username      => $username,
+            first_name    => $first_name,
+            last_name     => $last_name,
+            email_address => $email_address,
+            password      => $password,
+            active        => $active,
+        }
+    );
+
+    # Adding to the roles
+    # $user->create_related( 'user_roles', { role_id => $role_id } );
+    $user->add_to_user_roles( { role_id => $role_id } );
+
+    # Return to the user list
+    $c->response->redirect( $c->uri_for( $self->action_for('user_list') ) );
+}
+
+=head2 base -> user_object -> user_delete
+
+Delete a user
+
+=cut
+
+sub user_delete : Chained('user_object') : PathPart('user_delete') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash->{user_object}->delete;
+    $c->response->redirect( $c->uri_for( $self->action_for('user_list') ) );
 }
 
 =head2 auto
