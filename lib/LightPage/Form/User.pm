521128bdddf34f807e3460f0ffc7a38ce83bf122
Tamas Molnar <stiron@gmail.com>
2015-12-08 21:48:21 +0100

    Removed an unnecessary line

--- lib/LightPage/Form/User.pm
+++ lib/LightPage/Form/User.pm
@@ -12,12 +12,12 @@ has_field 'first_name'    => ( type => 'Text' );
 has_field 'last_name'     => ( type => 'Text' );
 has_field 'email_address' => ( type => 'Email', required => 1 );
 has_field 'active' => (
-    type    => 'Select',
+    type     => 'Select',
     options => [ { value => 0, label => 'No' }, { value => 1, label => 'Yes' } ]
 );
-( type => 'Integer', range_start => 0, range_end => 1, required => 1 );
-has_field 'roles'  => ( type => 'Multiple', label_column => 'role' );
-has_field 'submit' => ( type => 'Submit',   value        => 'Submit' );
+has_field 'roles' =>
+  ( type => 'Multiple', label_column => 'role', required => 1 );
+has_field 'submit' => ( type => 'Submit', value => 'Submit' );
 
 __PACKAGE__->meta->make_immutable;
 1;
