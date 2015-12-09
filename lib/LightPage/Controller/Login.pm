6506b7fc213bd12b8549891eb952179f59b270a6
Tamas Molnar <stiron@gmail.com>
2015-12-09 20:51:18 +0100

    Only active users can login modified:   lib/LightPage/Controller/Login.pm

--- lib/LightPage/Controller/Login.pm
+++ lib/LightPage/Controller/Login.pm
@@ -37,7 +37,8 @@ sub index : Path : Args(0) {
             $c->authenticate(
                 {
                     username => $username,
-                    password => $password
+                    password => $password,
+                    active   => 1,
                 }
             )
           )
